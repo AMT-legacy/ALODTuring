849e65f1145bb9aad0d62508e048821b3a8a74fd
FedericaBonifazi <92318256+federicabonifazi@users.noreply.github.com>
2022-04-03 01:23:19 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -160,3 +160,19 @@ magnify("myimage2", 2);
 magnify("myimage3", 3);
 magnify("myimage4", 7);
 magnify("myimage5", 2);
+
+/*MODALS ZOOM IMAGE*/
+var modals = document.getElementsByClassName('modalmyModal');
+// Get the button that opens the modal
+var btns = document.getElementsByClassName("openmodalmyBtn");
+var spans=document.getElementsByClassName("close");
+for(let i=0;i<btns.length;i++){
+    btns[i].onclick = function() {
+        modals[i].style.display = "block";
+    }
+}
+for(let i=0;i<spans.length;i++){
+    spans[i].onclick = function() {
+        modals[i].style.display = "none";
+    }
+}
